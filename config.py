8069d37a43e5444d6a4149110a7387050aae3fdb
Bernard Mairura <69302758+BernardMairura@users.noreply.github.com>
2020-11-05 11:31:31 +0300

    Role (#2)
    * add primary and username to user class
    
    * typos on manage.py
    
    * adding table column of roles and foreign key for one to many relationship
    
    * virtual restructure

---
+++ config.py
@@ -0,0 +1,47 @@
+import os
+
+ 
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
+    
+    
+
+
+
+class ProdConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
+    
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bernard:86kerubo19@localhost/bursary'
+    
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bernard:86kerubo19@localhost/bursary'
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
